62c23a0c6eb5123124b0ca14dfb4e3ca9a38490f
phasuwut <44774302+phasuwut@users.noreply.github.com>
2021-10-19 01:07:57 +0700

    addhowto

--- project_v1/RIP_v2/src/Router/A/Client.java
+++ project_v1/RIP_v2/src/Router/A/Client.java
@@ -1,7 +1,50 @@
 package Router.A;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.*;
 
+public class Client {
+    DatagramSocket Socket;
+
+    public Client() {
+
+    }
+
+    public void createAndListenSocket() {
+        try {
+
+            Socket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] incomingData = new byte[1024];
+            Student student = new Student(1, "Bijoy", "Kerala");
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ObjectOutputStream os = new ObjectOutputStream(outputStream);
+            os.writeObject(student);
+            byte[] data = outputStream.toByteArray();
+            DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, 9876);
+            Socket.send(sendPacket);
+            System.out.println("Message sent from client");
+            DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+            Socket.receive(incomingPacket);
+            String response = new String(incomingPacket.getData());
+            System.out.println("Response from server:" + response);
+            Thread.sleep(2000);
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.createAndListenSocket();
     }
 }
--- project_v1/RIP_v2/src/Router/A/Server.java
+++ project_v1/RIP_v2/src/Router/A/Server.java
@@ -1,7 +1,65 @@
 package Router.A;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+
+// how to http://www.java2s.com/example/java/network/udp-send-object.html
 public class Server {
-    public static void main(String[] args) {
+    DatagramSocket socket = null;
+
+    public Server() {
 
     }
+
+    public void createAndListenSocket() {
+        try {
+            socket = new DatagramSocket(9876);
+            byte[] incomingData = new byte[1024];
+
+            while (true) {
+                DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+                socket.receive(incomingPacket);
+                byte[] data = incomingPacket.getData();
+                ByteArrayInputStream in = new ByteArrayInputStream(data);
+                ObjectInputStream is = new ObjectInputStream(in);
+                try {
+                    Student student = (Student) is.readObject();
+                    System.out.println("Student object received = "+student);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+
+
+                InetAddress IPAddress = incomingPacket.getAddress();
+                int port = incomingPacket.getPort();
+                String reply = "Thank you for the message";
+                byte[] replyBytea = reply.getBytes();
+                DatagramPacket replyPacket =
+                        new DatagramPacket(replyBytea, replyBytea.length, IPAddress, port);
+                socket.send(replyPacket);
+                //Thread.sleep(2000);
+               // System.exit(0);
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.createAndListenSocket();
+    }
 }
+
